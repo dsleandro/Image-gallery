074033e9dfdb54962011c75076f3e436814f9c4a
Leandro.DS <dse.leandro@gmail.com>
2020-05-22 15:23:33 -0300

    Created maven project

---
+++ src/test/java/com/dsleandro/imagegallery/ImageGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsleandro.imagegallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
