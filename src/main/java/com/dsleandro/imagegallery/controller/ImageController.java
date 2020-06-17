6d9f67ee78c890605a9b820458a07bcae67926c1
Leandro.DS <dse.leandro@gmail.com>
2020-06-17 16:35:48 -0300

    upload image with form and save path

--- src/main/java/com/dsleandro/imagegallery/controller/ImageController.java
+++ src/main/java/com/dsleandro/imagegallery/controller/ImageController.java
@@ -1,8 +1,14 @@
 package com.dsleandro.imagegallery.controller;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.security.Principal;
 import java.util.stream.Collectors;
 
+import com.dsleandro.imagegallery.entity.Image;
+import com.dsleandro.imagegallery.entity.User;
+import com.dsleandro.imagegallery.repository.ImageRepository;
+import com.dsleandro.imagegallery.repository.UserRepository;
 import com.dsleandro.imagegallery.storage.StorageService;
 
 import org.springframework.core.io.Resource;
@@ -25,40 +31,62 @@ public class ImageController {
     @Autowired
     private StorageService storageService;
 
+    @Autowired
+    private ImageRepository imageRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
 
     @GetMapping("/")
-	public String displayImages(Model model) throws IOException {
+    public String displayImages(Model model) throws IOException {
 
-		model.addAttribute("images", storageService.loadAll().map(
-				path -> MvcUriComponentsBuilder.fromMethodName(ImageController.class,
-						"serveImage", path.getFileName().toString()).build().toUri().toString())
-				.collect(Collectors.toList()));
+        model.addAttribute("images",
+                storageService.loadAll()
+                        .map(path -> MvcUriComponentsBuilder
+                                .fromMethodName(ImageController.class, "serveImage",
+                                path.getFileName().toString())
+                                .build().toUri().toString())
+                        .collect(Collectors.toList()));
 
-		return "index";
-	}
+        return "index";
+    }
 
     @GetMapping("/images/{filename:.+}")
-	@ResponseBody
-	public ResponseEntity<Resource> serveImage(@PathVariable String filename) {
+    @ResponseBody
+    public ResponseEntity<Resource> serveImage(@PathVariable String filename) {
 
-		Resource file = storageService.loadAsResource(filename);
-		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
-	}
-  
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
+
     @PostMapping("/")
-    public String imageUpload(@RequestParam("file") MultipartFile file) {
+    public String imageUpload(@RequestParam("file") MultipartFile file, Principal user) {
 
         if (isImage(file)) {
 
+            // save image at server
             storageService.store(file);
+
+            // save path of image at database
+            try {
+                //parse MultipartFile to Image entity
+                Image image = parseToImage(file, user);
+
+                imageRepository.save(image);
+
+            } catch (Exception e) {
+                return "error";
+            }
 
             return "redirect:/";
 
         } else {
             return "error";
-        } 
-       
+        }
+
     }
 
     public boolean isImage(MultipartFile file) {
@@ -69,6 +97,17 @@ public class ImageController {
             return true;
         else
             return false;
+
+    }
+
+    public Image parseToImage(MultipartFile file, Principal user) throws Exception {
+
+        Path path = storageService.getPath(file.getOriginalFilename());
+
+        User username = userRepository.findByUsername(user.getName())
+                .orElseThrow(() -> new Exception("user not found"));
+
+        return new Image(path.toString(), username);
 
     }
 }
